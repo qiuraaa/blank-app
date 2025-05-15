25dc5533deac5216edde5cb60f885187b17fc69c
qiuraaa <kekeykey320@gmail.com>
2025-05-15 14:34:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Selamat datang di Website Informatika")
+st.image("20250513_095138.jpg", width=200)
+st.write("\n")
+st.subheader("Keyra Destianti Alrassya")
+st.writer(
+    Mari bermakan mochi sambil membuat Website melalui Github dan Streamlit 
